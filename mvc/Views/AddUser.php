472d5ee2586e4257838f76bedaaa485061fcf29b
Roman Developer <romzes13@gmail.com>
2018-03-11 11:53:01 -0400

    Adds view for user

---
+++ mvc/Views/AddUser.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+ <head>
+
+    <title>Add new user::</title>
+
+
+</head>
+<body>
+
+
+    <form method="post" action="newUser">
+    <label id="first"> First name:</label><br/>
+    <input type="text" name="userName"><br/>
+
+    <label id="first">Password</label><br/>
+    <input type="password" name="password"><br/>
+
+    <label id="first">Role</label><br/>
+    <input type="text" name="role"><br/>
+
+  <!--  <button type="submit" name="save">save</button>
+    <button type="submit" name="get">get</button>
+ -->
+        <input type=submit>
+            <input type=reset>
+    </form>
+
+</body>
+</html>
