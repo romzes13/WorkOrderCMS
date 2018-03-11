e93b8960fe8a28eb2df1d5fa6f8499f2f65259e5
Roman Developer <romzes13@gmail.com>
2018-03-11 18:22:02 -0400

    Creates view for workorder

---
+++ mvc/Views/AddWorkorder.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+ <head>
+
+    <title>Add new workorder::</title>
+
+
+</head>
+<body>
+
+    <form method="post" action="newWorkorder">
+
+    <label id="first"> Description:</label><br/>
+    <input type="text" name="description"><br/>
+
+    <label id="first">Estimate</label><br/>
+    <input type="text" name="estimate"><br/>
+
+    <label id="first">Location</label><br/>
+    <input type="text" name="location"><br/>
+
+    <label id="first"> Received:</label><br/>
+    <input type="text" name="received"><br/>
+
+    <label id="first">Scheduled:</label><br/>
+    <input type="text" name="scheduled"><br/>
+
+    <label id="first">Compleated?</label><br/>
+    <input type="text" name="compleated"><br/>
+
+    <label id="first">Location id:</label><br/>
+    <input type="text" name="location_id"><br/>
+
+  <!--  <button type="submit" name="save">save</button>
+    <button type="submit" name="get">get</button>
+ -->
+        <input type=submit>
+            <input type=reset>
+    </form>
+
+</body>
+</html>
