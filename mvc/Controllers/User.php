2d7fd1b5d0935262586782e755551fcadd2c0c02
Roman Developer <romzes13@gmail.com>
2018-04-04 00:56:21 -0400

    Adds listUsers( )

--- mvc/Controllers/User.php
+++ mvc/Controllers/User.php
@@ -4,7 +4,17 @@ class User extends Controller {
 
     public static function listUsers() {
            // print_r(self::query("SELECT * FROM Handymans_list"));
+        echo "<h4>Inside listUsers() </h4>";
         print_r(self::query("SELECT * FROM users"));
+
+        $data = self::query("SELECT * FROM users");
+
+    // set the resulting array to associative
+
+    //$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($data->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
 
     }
 
