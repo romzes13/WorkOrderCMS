942183c4c8f44e7631f435ca00314f524d46a481
Roman Developer <romzes13@gmail.com>
2018-04-04 01:03:47 -0400

    Iterates thru array and displays each row

--- mvc/Views/AboutUs.php
+++ mvc/Views/AboutUs.php
@@ -1,1 +1,17 @@
 <h1>About Us</h1>
+
+<?php
+
+//$data = AboutUs::test();
+$data = AboutUs::printUsers();
+
+echo "<br> ID  Name  Password  Role<br>";
+// Printing objects from an array
+foreach( $data as $row ) {
+         echo $row['id']."--";
+         echo $row['name']."  --";
+         echo $row['password']."  ";
+         echo $row['role']."  <br>";
+}
+
+?>
