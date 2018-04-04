3416feab2d8932fddcf4f0e84f8fb4c9ebfb4d0c
Roman Developer <romzes13@gmail.com>
2018-04-04 16:15:04 -0400

    Adds foreach loop and prints variables

--- mvc/Views/AboutUs.php
+++ mvc/Views/AboutUs.php
@@ -5,13 +5,36 @@
 //$data = AboutUs::test();
 $data = AboutUs::printUsers();
 
-echo "<br> ID  Name  Password  Role<br>";
-// Printing objects from an array
-foreach( $data as $row ) {
-         echo $row['id']."--";
-         echo $row['name']."  --";
-         echo $row['password']."  ";
-         echo $row['role']."  <br>";
-}
+    echo "<br> ID  Name  Password  Role<br>";
+    // Printing objects from an array
+    foreach( $data as $row ) {
+             echo $row['id']."--";
+             echo $row['name']."  --";
+             echo $row['password']."  ";
+             echo $row['role']."  <br>";
+    }
+
+    $user = new User("name1", "1234", "admin");
+
+    $user->getUser();
+    echo $user->password."<br>";
+    echo $user->role."<br>";
+
+    echo "<br><h5>Find user:</h5>";
+    $name = UserImpl::findUser("Roland");
+
+
+
+    foreach( $name as $row ) {
+             echo $row['id']."--";
+             echo $row['name']."  --";
+             echo $row['password']."  ";
+             echo $row['role']."  <br>";
+    }
+
+
+
+
+
 
 ?>
