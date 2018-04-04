d1b26f972cd120d37b112667af0d0c17c504048d
Roman Developer <romzes13@gmail.com>
2018-04-04 00:55:35 -0400

    Adds new method printUsers()

--- mvc/Controllers/AboutUs.php
+++ mvc/Controllers/AboutUs.php
@@ -8,6 +8,12 @@ class AboutUs extends Controller {
 
     }
 
+    public static function printUsers() {
+           // print_r(self::query("SELECT * FROM Handymans_list"));
+        return (self::query("SELECT * FROM users"));
+
+    }
+
 }
 
 ?>
