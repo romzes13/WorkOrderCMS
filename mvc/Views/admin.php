8f2b83b06a7d608baa0689fd2edaeab0a5c88522
Roman Developer <romzes13@gmail.com>
2018-04-09 10:52:09 -0400

    Admins panel

---
+++ mvc/Views/admin.php
@@ -0,0 +1,9 @@
+<nav>
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="gallery.html">Gallery</a></li>
+    <li><a href="services.html">Services</a></li>
+    <li><a href="feedback.html">Feedback</a></li>
+    <li><a href="contacts.html">Contacts</a></li>
+  </ul>
+</nav>
