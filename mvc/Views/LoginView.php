e02826a81f2e6ac5ab1f5ee6d53e509ba9469be8
Roman Developer <romzes13@gmail.com>
2018-04-09 10:49:37 -0400

    Adds Login implementation

---
+++ mvc/Views/LoginView.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title>Workorder CMS</title>
+    <meta charset="utf-8">
+   <!-- <link rel="stylesheet" href="/mvc/CSS/styles.css"> -->
+    <style>
+    <?php include 'CSS/styles.css'; ?>
+    </style>
+
+</head>
+
+
+<body>
+    <div id="wrapper">
+<header><h2>User login page</h2></header>
+
+<nav>
+    <b>
+
+    </b>
+
+</nav>
+
+    <main>
+
+
+        <h4>* Required fields</h4>
+
+    <!-- Form for Help -->
+<form action="loginAuth" method="post">
+            <!-- form controlls -->
+
+<fieldset>
+    <legend>Enter   name and password</legend>
+<div >
+<label id="label1">Name:</label><input name="userName" type="text" >
+<h8>*</h8><h8 id="errorName" class="error"></h8>
+
+<label id="label1">Password: </label><input name="password" type="text" > *
+<h8 id="errorPassw" class="error"></h8>
+
+
+</div>
+</fieldset>
+
+            <div id="custom1">
+
+            <input type=submit>
+            <input type=reset>
+             </div>
+
+        </form>
+
+    <br>
+        <footer> <small>Copyright &copy; 2018 Roman Babushkin</small></footer>
+
+        </main>
+</div>
+</body>
+</html>
