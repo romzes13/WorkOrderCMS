29eb4983f1cbf5b4f9b24334324f6eb86346c71b
Roman Developer <romzes13@gmail.com>
2018-04-09 13:10:39 -0400

    new contractor's dashboard

---
+++ mvc/Views/contractor_dashboard.php
@@ -0,0 +1,9 @@
+<nav>
+  <ul>
+    <li><a href="">Home</a></li>
+    <li><a href="loginTest">Session</a></li>
+    <li><a href="listWorkorders">Display all workorders</a></li>
+    <li><a href="dashboard">Dashboard</a></li>
+    <li><a href="logout">logout</a></li>
+  </ul>
+</nav>
