d84f2b5675ea88db9a09a4d9b4674310db0d968c
Roman Developer <romzes13@gmail.com>
2018-04-09 23:47:17 -0400

    Removed useless code and added  contractorWorkorders method

--- mvc/Controllers/WorkorderImpl.php
+++ mvc/Controllers/WorkorderImpl.php
@@ -8,24 +8,26 @@ class WorkorderImpl extends Controller {
 
     }
 
+        // Finish it TODO
+        public static function contractorWorkordersById($id) {
+
+        return (self::query("SELECT * FROM workorder"));
+
+    }
+
+
+
     public static function addWorkorder($description, $estimate, $location, $received,
         $scheduled, $compleated, $location_id) {
 
-        // remove echo statements
-        //echo "user: ". $name;
-        echo "<br>";
-        //echo "password: ".$password;
-        echo "<br>";
 
         //TODO Add security and validate input
 
 $sql ="INSERT INTO workorder (description, estimate, location, received,
         scheduled, compleated, location_id) VALUES ('$description', '$estimate', '$location', '$received', '$scheduled', '$compleated', '$location_id')";
 
-
         self::insert($sql);
 
-        //echo $sql;
 
    //$query = $db->prepare( $sql );
    // $query->execute( array( ':username'=>$name, ':password'=>$password));
