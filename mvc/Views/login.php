c53e6e12bfb0ad06cd8223bb2dcd206532527dcc
Roman Developer <romzes13@gmail.com>
2018-04-11 22:19:58 -0400

    Updated login form

---
+++ mvc/Views/login.php
@@ -0,0 +1,26 @@
+   <h4>* Required fields</h4>
+
+    <!-- Form for Help -->
+<form action="loginAuth" method="post">
+            <!-- form controlls -->
+
+<fieldset>
+    <legend>Enter   name and password</legend>
+<div >
+<label id="label1">Name:</label><input name="userName" type="text" >
+<h8>*</h8><h8 id="errorName" class="error"></h8>
+
+<label id="label1">Password: </label><input name="password" type="text" > *
+<h8 id="errorPassw" class="error"></h8>
+
+
+</div>
+</fieldset>
+
+            <div id="custom1">
+
+            <input type=submit>
+            <input type=reset>
+             </div>
+
+        </form>
