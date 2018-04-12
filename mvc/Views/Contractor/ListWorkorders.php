6730221d65d9532e6fe3f76b10c12b6ebfb350b5
Roman Developer <romzes13@gmail.com>
2018-04-11 22:14:18 -0400

    Lists all workorders

---
+++ mvc/Views/Contractor/ListWorkorders.php
@@ -0,0 +1,35 @@
+<h4>Workorders::</h4>
+
+        <table>
+        <caption>List of all workorders</caption>
+            <tr>
+                <th>Id</th>
+                <th>Description</th>
+                <th>Estimate</th>
+                <th>Location</th>
+                <th>Received</th>
+                <th>Scheduled   </th>
+                <th>Compleated</th>
+                <th>Location_id</th>
+                <th>Accept?</th>
+            </tr>
+  <?php
+            $data = WorkorderImpl::listWorkorders();
+
+            foreach( $data as $row ) {
+
+            echo "<tr>";
+            echo "<th>".$row['id']."</th>";
+            echo "<th>".$row['description']."</th>";
+            echo "<th>".$row['estimate']."</th>";
+            echo "<th>".$row['location']."</th>";
+            echo "<th>".$row['received']."</th>";
+            echo "<th>".$row['scheduled']."</th>";
+            echo "<th>".$row['compleated']."</th>";
+            echo "<th>".$row['location_id']."</th>";
+            echo "<th><a href=\"acceptWorkorder?id=".$row['id']."\">accept wo</a></th>";
+            echo "</tr>";
+            }
+
+        echo "</table>";
+    ?>
