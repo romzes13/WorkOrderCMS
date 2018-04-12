0e1d3ee05a0425bb119538e99a964240aa4a42bc
Roman Developer <romzes13@gmail.com>
2018-04-11 22:18:12 -0400

    Updates with a new view page

--- mvc/Views/Index.php
+++ mvc/Views/Index.php
@@ -1,6 +1,39 @@
-<h1>Welcome!</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title>Workorder CMS</title>
+    <meta charset="utf-8">
+   <!-- <link rel="stylesheet" href="/mvc/CSS/styles.css"> -->
+    <style>
+    <?php include 'CSS/styles.css'; ?>
+    </style>
+
+</head>
 
 
+<body>
+    <div id="wrapper">
+<main>
+        <?php      include 'header.php'; ?>
+
+    <nav>
+    <ul>
+       <li><a href="">Home</a></li>
+<li><a href="about-us">about us</a></li>
+<li><a href="login">User login</a></li>
+<li><a href="dashboard">Dashboard</a></li>
+<li><a href="logout">User logout</a></li>
+    </ul>
+
+</nav>
+
+<h3>Welcome to Workorder CMS!</h3>
+        <p>Please login using the form located below or  <a href="http://localhost/mvc/addUser">click here</a> to register if you are a new user.</p>
+
+
+<?php      include 'login.php'; ?>
+
+        <br><br>
 <a href="">Home</a><br>
 <a href="about-us">about us</a><br>
 <a href="login">User login</a><br>
@@ -12,3 +45,12 @@
 <a href="addWorkorder">Add workorder</a><br>
 <a href="listWorkorders">Display all workorders</a><br>
 
+
+
+    <br>
+        <?php      include 'footer.php'; ?>
+
+        </main>
+</div>
+</body>
+</html>
