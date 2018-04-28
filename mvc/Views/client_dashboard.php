e41aad5bfc96c407b12d3ee14274ed569a49320e
Roman Developer <romzes13@gmail.com>
2018-04-28 11:46:47 -0400

    Includes new links in clients dashboard

--- mvc/Views/client_dashboard.php
+++ mvc/Views/client_dashboard.php
@@ -1,10 +1,12 @@
 <nav>
   <ul>
-    <li><a href="">Home</a></li>
+    <li><a href="index.php">Home</a></li>
     <li><a href="sessionTest">Session</a></li>
+     <li><a href="listUsers1">Users</a></li>
     <li><a href="addWorkorder1">New workorder</a></li>
     <li><a href="listWorkorders1">Display all workorders</a></li>
     <li><a href="dashboard">Dashboard</a></li>
+    <li><a href="about">About us</a></li>
     <li><a href="logout">logout</a></li>
   </ul>
 </nav>
