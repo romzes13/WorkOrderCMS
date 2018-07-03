0a44306ff54961eda8b2d96b03b4391d15d25154
Roman Developer <romzes13@gmail.com>
2018-07-03 10:35:22 -0400

    Adds test for name

---
+++ mvc/tests/ContractorTest.php
@@ -0,0 +1,31 @@
+<?php
+require './classes/Contractor.php';
+
+class ContractorTests extends PHPUnit_Framework_TestCase
+{
+
+/*    private $contractor;
+
+    protected function setUp()
+    {
+        $this->contractor = new Contractor("Roman", "address", "phone", "email", "rate", "user_id");
+    }
+
+    protected function tearDown()
+    {
+        $this->contractor = NULL;
+    }
+ */
+    public function testName()
+    {
+       // $contractor = new ContractorTests();
+        //$contractor->setUp();
+        //$contractor->name = "Roman";
+       $contractor = new Contractor("Roman", "address", "phone", "email", "rate", "user_id");
+
+        //$result = $this->contractor->name('Roman');
+        $this->assertEquals($contractor->getName(), 'Roman');
+    }
+
+
+}
