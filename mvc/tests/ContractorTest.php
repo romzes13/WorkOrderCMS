e3051e52b13aee1da1fcd84fb46a49d14f1ee520
Roman Babushkin <romzes13@users.noreply.github.com>
2018-07-03 12:56:22 -0400

    Update ContractorTest.php

--- mvc/tests/ContractorTest.php
+++ mvc/tests/ContractorTest.php
@@ -1,9 +1,13 @@
 <?php
 require '/home/travis/build/romzes13/WorkOrderCMS/mvc/classes/Contractor.php';
 //require './classes/Contractor.php';
+// use the following namespace
+use PHPUnit\Framework\TestCase;
 
-class ContractorTest extends PHPUnit_Framework_TestCase
-{
+// extend using TestCase instead PHPUnit_Framework_TestCase
+class ContractorTest extends TestCase {
+//class ContractorTest extends PHPUnit_Framework_TestCase
+
 
 /*    private $contractor;
 
