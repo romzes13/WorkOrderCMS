66e24eb767f01fce7de1127b3a7af20e437a6e36
Roman Developer <romzes13@gmail.com>
2018-08-01 13:06:02 -0400

    Updates Observer pattern

--- mvc/classes/Observer/ActivityStream.php
+++ mvc/classes/Observer/ActivityStream.php
@@ -7,6 +7,18 @@ class ActivityStream
        // echo "<br> ActivityStream: method addNewItem() called";
         print $item;
     }
+
+    /*
+    *   Updates record in database with an $id
+    *
+    */
+    public static function addRecord($lastId, $item)
+    {
+       //include_once("../Controllers/NotificationImpl.php");
+        include '../mvc/Controllers/NotificationImpl.php';
+       NotificationImpl::setNotificationText($lastId, $item);
+        print $item;
+    }
 }
 
 ?>
